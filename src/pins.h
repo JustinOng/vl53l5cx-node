e8c2beb3ae3a0f3b6bf2fd9cc785e0348582d273
Justin Ong <9336475+JustinOng@users.noreply.github.com>
2023-07-30 17:42:23 +0800

    Increase comms speed

--- src/pins.h
+++ src/pins.h
@@ -12,7 +12,7 @@ constexpr int PIN_TXD = 8;
 constexpr int PIN_RXD = 7;
 
 constexpr int UART_NUM = 1;
-constexpr int UART_BAUD = 115200;
+constexpr int UART_BAUD = 921600;
 constexpr int UART_RX_BUFFER_LEN = 256;
 constexpr int UART_RX_PAT_CHR = 0xA5;
 constexpr int UART_RX_PAT_COUNT = 4;
